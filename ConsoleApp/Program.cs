2d3a1288819914f8eaa73668196d08bc635415ee
Valentine Podglazova <120104181+Valentina51R@users.noreply.github.com>
2022-12-15 00:24:00 +0700

    Add files via upload

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Model;
+using System;
+
+namespace ConsoleApp
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Person percon = new Person(null, "Ivanov", 20, Gender.Male);
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            
+
+            Person partner = new Person("Anastasha", "Ivanova", 40, Gender.Female);
+
+            //percon.Parter = partner;
+            /*
+            for(int i = 0; i < 20; i++)
+            {
+                percon.CelebrateHappyBirthday();
+            }
+            */
+            // Console.WriteLine(percon.GetInfo());
+            Console.ReadKey();
+        }
+    }
+}
